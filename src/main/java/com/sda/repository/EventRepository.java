540f505afc8ba456b6c584e84fda500eb30289fc
Denis <denis.leping@gmail.com>
2022-12-17 13:59:43 +0200

    Few minor changes to user, Event class

---
+++ src/main/java/com/sda/repository/EventRepository.java
@@ -0,0 +1,7 @@
+package com.sda.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository {
+}
--- src/main/java/com/sda/repository/UserRepository.java
+++ src/main/java/com/sda/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.sda.repository;
 
 import com.sda.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository  extends CrudRepository<User, Integer> {
 }
