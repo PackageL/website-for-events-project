8b8844fb584df15959a38e8056600e773e016e46
DenisYolo <denis.leping@yolo.com>
2022-12-25 17:08:49 +0200

    signin and index page added.

--- src/main/java/com/sda/controller/UserController.java
+++ src/main/java/com/sda/controller/UserController.java
@@ -2,9 +2,14 @@ package com.sda.controller;
 
 import com.sda.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
+@Controller
 public class UserController {
 
     private UserRepository repository;
@@ -13,4 +18,20 @@ public class UserController {
     public UserController(UserRepository repository) {
         this.repository = repository;
     }
+
+    @GetMapping("/")
+    public String homePage() {
+        return "index";
+    }
+
+    @GetMapping("/eventList")
+    public String eventList() {
+        return "event-list";
+    }
+
+    @GetMapping("/signin")
+    public String signin() {
+        return "signin";
+    }
+
 }
--- src/main/java/com/sda/repository/EventRepository.java
+++ src/main/java/com/sda/repository/EventRepository.java
@@ -4,6 +4,12 @@ import com.sda.model.Event;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EventRepository extends CrudRepository<Event, Integer> {
+
+    List<Event> findByTitle(String title);
+
+    List<Event> findAll();
 }
