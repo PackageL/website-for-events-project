a64b18c10667b415cde41e7b49113cf0eb4b4a68
PackageL <99537635+PackageL@users.noreply.github.com>
2023-01-06 15:15:37 +0200

    Added: event-list.html user-events.html view-event.html
    Made adjustments in:
    create-event.html - added base code
    Event.java - change data type to LocalData
    event-list.html - added some base code
    EventController.java - added mappings
    index.html - made few adjustments
    signin.html - made few adjustments
    UserController.java - removed Event mappings

--- src/main/java/com/sda/controller/EventController.java
+++ src/main/java/com/sda/controller/EventController.java
@@ -1,8 +1,14 @@
 package com.sda.controller;
 
+import com.sda.model.Event;
 import com.sda.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class EventController {
@@ -12,5 +18,28 @@ public class EventController {
     @Autowired
     public EventController(EventRepository repository) {
         this.repository = repository;
+    }
+
+    @PostMapping("/createAnEvent")
+    public String createEvent(@RequestParam("title") String title,
+                              @RequestParam("date") LocalDate date,
+                              @RequestParam("description") String description) {
+        Event event = new Event(title, date, description);
+        repository.save(event);
+        return "create-event";
+    }
+
+    @GetMapping("/events/{id}")
+    public String viewEvent(@PathVariable("id") int id, Model model) {
+        Optional<Event> event = repository.findById(id);
+        model.addAttribute("event", event);
+        return "viewEvent";
+    }
+
+    @GetMapping("/events")
+    public String listEvents(Model model) {
+        List<Event> events = repository.findAll();
+        model.addAttribute("events", events);
+        return "event-list";
     }
 }
--- src/main/java/com/sda/controller/UserController.java
+++ src/main/java/com/sda/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.sda.controller;
 
+import com.sda.model.Event;
 import com.sda.model.User;
 import com.sda.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,10 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -27,11 +30,6 @@ public class UserController {
         return "index";
     }
 
-    @GetMapping("/eventList")
-    public String eventList() {
-        return "event-list";
-    }
-
     @GetMapping("/signin")
     public String signin() {
         return "signin";
@@ -44,14 +42,5 @@ public class UserController {
         return "signup";
     }
 
-    @PostMapping("/register")
-    public String registerUser(@Valid User user, BindingResult result) {
-        if (result.hasErrors()) {
-            return "signup";
-        }
-
-        repository.save(user);
-        return "redirect:/signup?success";
-    }
 
 }
