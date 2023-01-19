469b2bc0af56fc4ecd92bc1f3047a736b0c57f0f
DenisYolo <denis.leping@yolo.com>
2023-01-19 21:28:13 +0200

    Anton pm

--- src/main/java/com/sda/service/impl/CommentServiceImpl.java
+++ src/main/java/com/sda/service/impl/CommentServiceImpl.java
@@ -4,17 +4,21 @@ import com.sda.model.Comment;
 import com.sda.repository.CommentRepository;
 import com.sda.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CommentServiceImpl implements CommentService {
     @Autowired
     private CommentRepository commentRepository;
 
+    @Override
     public List<Comment> getCommentsByEventId(long eventId) {
         return commentRepository.findByEventId(eventId);
     }
 
+    @Override
     public void addComment(Comment comment) {
         commentRepository.save(comment);
     }
